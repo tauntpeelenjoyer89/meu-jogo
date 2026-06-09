8a98be826cb6d81fec40cb48c833b1e4ebbee2e4
tauntpeelenjoyer89 <h3llor4nothe3re@gmail.com>
2026-06-09 16:35:59 -0300

    sistema de batalha criado

---
+++ batalha.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+void exibir(string nome, int vida, int ataque) {
+	
+	cout << "--- " << nome << " ---" << enl;
+	cout << "vida: " << vida << endl;
+	cout << "Ataque: " << ataque << endl;
+	
+	
+}
+
+int dano (int ataque, int defesa) {
+	
+	int d = ataque - defesa;
+	return (d > 0) ? d : 0;
+}
